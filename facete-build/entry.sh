dc48ceac2e6e1ee378f47e763cb9a92f02d27b43
Kirill Bulert <bulert@infai.org>
2023-08-24 13:38:20 +0200

    Remove old bootstrap process

--- facete-build/entry.sh
+++
@@ -1,3 +0,0 @@
-git pull
-mvn clean install -f pom.xml -D skipTests -P production -pl :facete3-pkg-app-docker-web jib:buildTar
-cp facete3-pkg-parent/facete3-pkg-docker-app-web/target/jib-image.tar /app/target/facete-docker-image.tar
